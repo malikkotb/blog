c3ecf26b8d336f338400500dfb6656cf775b8bf9
Malik Kotb <malikkotb@icloud.com>
2024-01-03 21:41:34 +0200

    queries and styling

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -9,6 +9,16 @@ import {
 import { UrlObject } from "url";
 import Image from "next/image";
 
+const convertDateFormat = (dateStr: string): string => {
+  const dateObj = new Date(dateStr);
+  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' };
+  return dateObj.toLocaleDateString('en-US', options);
+};
+
+const dateExample = "2023-12-31";
+const convertedDate = convertDateFormat(dateExample);
+console.log(convertedDate);
+
 export default function Posts({ posts }: any) {
   return (
     // <div className="w-full flex flex-col sm:flex-wrap gap-6 sm:gap-8">
@@ -42,25 +52,43 @@ export default function Posts({ posts }: any) {
 
           return (
             <Link
-              className="flex flex-col h-full border p-4 rounded-2xl"
+              className="flex flex-col h-[45vh] p-2 rounded-2xl"
               href={post.slug}
               key={post.id}
             >
-              <div>
-                <Image
-                  src={coverPhoto?.url}
-                  width={150}
-                  height={100}
-                  alt="cover photo"
+              <div className="w-full h-[18vw]">
+                <img
+                  src={coverPhoto.url}
+                  className="w-full h-full rounded-xl object-cover"
+                  alt="description"
                 />
-                <h3 className="text-xl">{title}</h3>
-                <p className="text-base">{datePublished}</p>
-                <p className="">{description}</p>
               </div>
+              {/* <div className="items-center flex">
+                <img
+                  src={coverPhoto.url}
+                  className="h-[18vw] w-[25vw] rounded-xl object-cover"
+                  alt="description"
+                />
+              </div> */}
+
+              <h3 className="text-xl">{title}</h3>
+              <p className="text-sm opacity-70">{convertDateFormat(datePublished as string)}</p>
+              <p className=" overflow-y-hidden">{description}</p>
             </Link>
           );
         }
       )}
     </div>
   );
+}
+
+{
+  /* <Image
+                src={coverPhoto?.url}
+                width={300}
+                height={200}
+                alt="cover photo"
+                style={{ objectFit: "fill" }}
+                sizes="(max-width: 300) 100vw, 33vw"
+              /> */
 }
