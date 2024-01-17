e675c144b88fd84521ca470177891fd08684a074
Malik Kotb <malikkotb@icloud.com>
2024-01-17 21:56:55 +0200

    styling syntax highlighting

--- components/CustomPre.tsx
+++ components/CustomPre.tsx
@@ -1,17 +1,21 @@
 "use client";
 import { ReactNode } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
+import { CopyIcon } from '@radix-ui/react-icons'
 import {
   docco,
   atelierDuneDark,
+  atelierCaveLight,
+  atelierCaveDark,
   googlecode,
+  arduinoLight,
 } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import React from "react";
 import { useTheme } from "next-themes";
 
 interface MDXComponentProps {
-    children?: ReactNode;
-  }
+  children?: ReactNode;
+}
 export default function CustomPre({ children, ...props }: MDXComponentProps) {
   const { theme } = useTheme();
 
@@ -21,29 +25,23 @@ export default function CustomPre({ children, ...props }: MDXComponentProps) {
     codeString = children.props.children;
   }
 
+  const style = theme === "dark" ? atelierDuneDark : atelierCaveDark;
 
   return (
     <>
-      {theme === "light" && (
-        <SyntaxHighlighter
-          className={"rounded-lg bg-transparent"}
-          showLineNumbers
-          language="typescript"
-          style={googlecode}
-        >
-          {codeString}
-        </SyntaxHighlighter>
-      )}
-      {theme === "dark" && (
-        <SyntaxHighlighter
-          className={"rounded-lg bg-transparent"}
-          showLineNumbers
-          language="typescript"
-          style={atelierDuneDark}
-        >
-          {codeString}
-        </SyntaxHighlighter>
-      )}
+      <div className="flex justify-between px-4 py-1 rounded-lg rounded-b-none border-b-0 border">
+        <p>Title</p>
+        <button className=""><CopyIcon  className="hover:text-green-500" /></button>
+      </div>
+      <SyntaxHighlighter
+        className={" border"}
+        showLineNumbers
+        language="typescript"
+        customStyle={{ background: "transparent", padding: "15px" }}
+        style={atelierCaveLight}
+      >
+        {codeString}
+      </SyntaxHighlighter>
     </>
   );
 }
